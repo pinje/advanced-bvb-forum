f21ce1efe0c049a43e06fd85cfa5e586355ca11d
Shuhei <shuhei7196artdelta@hotmail.com>
2024-04-04 03:01:32 +0200

    add mock services

---
+++ services/backend/gateway/src/test/java/com/bvb/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bvb.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/backend/school/src/main/java/com/bvb/school/School.java
@@ -0,0 +1,23 @@
+package com.bvb.school;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class School {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
