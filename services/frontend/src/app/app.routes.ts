1130b948694fe71218847ad571a4808676138799
Shuhei <shuhei7196artdelta@hotmail.com>
2024-04-18 02:08:21 +0200

    navbar basics

--- services/frontend/src/app/app.routes.ts
+++ services/frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'signup', component: SignupComponent}
+];
