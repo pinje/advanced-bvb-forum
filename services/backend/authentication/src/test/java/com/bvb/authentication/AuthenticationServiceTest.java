cbada9d0bbc6725cec80e03a6d7ef837ab96a449
Shuhei Hagiwara <498855@student.fontys.nl>
2024-04-22 03:16:07 +0200

    Add authentication unit tests

--- services/backend/authentication/src/test/java/com/bvb/authentication/AuthenticationServiceTest.java
+++ services/backend/authentication/src/test/java/com/bvb/authentication/AuthenticationServiceTest.java
@@ -3,6 +3,9 @@ package com.bvb.authentication;
 import com.bvb.authentication.business.AuthenticationService;
 import com.bvb.authentication.business.exception.EmailAlreadyExistsException;
 import com.bvb.authentication.business.exception.UsernameAlreadyExistsException;
+import com.bvb.authentication.config.security.JwtService;
+import com.bvb.authentication.domain.AuthenticationRequest;
+import com.bvb.authentication.domain.AuthenticationResponse;
 import com.bvb.authentication.domain.RegistrationRequest;
 import com.bvb.authentication.persistence.Role;
 import com.bvb.authentication.persistence.RoleRepository;
@@ -14,9 +17,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,6 +39,12 @@ public class AuthenticationServiceTest {
     private AuthenticationService authenticationService;
     @Mock
     private PasswordEncoder passwordEncoder;
+    @Mock
+    private JwtService jwtService;
+    @Mock
+    private AuthenticationManager authenticationManager;
+    @Mock
+    private Authentication authentication;
 
     @Test
     void Add_ValidUser_UserSavedInRepository() {
@@ -109,5 +122,33 @@ public class AuthenticationServiceTest {
         assertEquals(HttpStatus.BAD_REQUEST, exception.getStatusCode());
         verify(userRepository, times(1)).existsByEmail(request.getEmail());
         verify(userRepository, times(1)).existsByUsername(request.getUsername());
+    }
+
+    @Test
+    void Login_ValidUser_ReturnJwtToken() {
+        // ARRANGE
+        AuthenticationRequest request = AuthenticationRequest.builder()
+                .email("user@gmail.com")
+                .password("12345678")
+                .build();
+
+        User user = User.builder()
+                .id(1L)
+                .email(request.getEmail())
+                .username("user123")
+                .password(request.getPassword())
+                .build();
+        when(authentication.getPrincipal()).thenReturn(user);
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(authentication);
+        when(jwtService.generateToken(anyMap(), any(User.class))).thenReturn("token");
+
+        // ACT
+        AuthenticationResponse response = authenticationService.authenticate(request);
+
+        // ASSERT
+        assertNotNull(response);
+        assertEquals("token", response.getToken());
+        verify(authenticationManager).authenticate(any(UsernamePasswordAuthenticationToken.class));
+        verify(jwtService).generateToken(anyMap(), any(User.class));
     }
 }
