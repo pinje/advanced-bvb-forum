171a5cd01dc3a3adac8a7c6e46461a6854af3345
Shuhei Hagiwara <498855@student.fontys.nl>
2024-05-03 05:04:51 +0200

    remove token from frontend

--- services/frontend/src/app/app.routes.ts
+++ services/frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SeasonsComponent } from './pages/seasons/seasons.component';
 
 export const routes: Routes = [
     { path: 'signup', component: SignupComponent},
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'seasons', component: SeasonsComponent }
 ];
