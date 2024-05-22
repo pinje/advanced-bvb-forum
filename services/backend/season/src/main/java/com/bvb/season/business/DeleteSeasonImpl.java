495796757f315f9eb612cff6c09922cb1a5b75f5
Shuhei Hagiwara <498855@student.fontys.nl>
2024-05-22 10:09:15 +0200

    idk

--- services/backend/season/src/main/java/com/bvb/season/business/DeleteSeasonImpl.java
+++ services/backend/season/src/main/java/com/bvb/season/business/DeleteSeasonImpl.java
@@ -1,16 +1,37 @@
 package com.bvb.season.business;
 
+import com.azure.messaging.servicebus.*;
+
+import com.bvb.season.SeasonApplication;
 import com.bvb.season.business.exception.SeasonDoesntExistsException;
 import com.bvb.season.persistence.Season;
 import com.bvb.season.persistence.SeasonRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
+@EnableJms
 public class DeleteSeasonImpl implements DeleteSeasonService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DeleteSeasonImpl.class);
+    private static final String TOPIC_NAME = "bvbforumtopic";
+    private static final String SUBSCRIPTION_NAME = "tournament-logo";
+
+    @Value("${spring.cloud.azure.servicebus.connection-string}")
+    private String connectionString;
+
+    @Value("${spring.cloud.azure.servicebus.consumer.entity-name}")
+    private String topicName;
+
     private final SeasonRepository seasonRepository;
 
     @Override
@@ -21,8 +42,20 @@ public class DeleteSeasonImpl implements DeleteSeasonService {
             throw new SeasonDoesntExistsException();
         }
 
+        ServiceBusSenderClient senderClient = new ServiceBusClientBuilder()
+                .connectionString(connectionString)
+                .sender()
+                .topicName(topicName)
+                .buildClient();
+        senderClient.sendMessage(new ServiceBusMessage("yea").setSubject("tournament-logo"));
+
         seasonRepository.deleteById(seasonId);
     }
 
-    // delete related matches
+    @JmsListener(destination = TOPIC_NAME, containerFactory = "topicJmsListenerContainerFactory",
+            subscription = SUBSCRIPTION_NAME)
+    public void receiveMessage(byte[] messageBytes) {
+        String message = new String(messageBytes, StandardCharsets.UTF_8);
+        LOGGER.info("Message received: {}", message);
+    }
 }
