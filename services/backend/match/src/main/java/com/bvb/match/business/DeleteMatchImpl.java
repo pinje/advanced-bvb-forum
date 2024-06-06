73bd63752d73f00ef5333c63d8ee906537c22bc4
Shuhei Hagiwara <498855@student.fontys.nl>
2024-06-06 13:21:11 +0200

    add messaging deletion for posts

--- services/backend/match/src/main/java/com/bvb/match/business/DeleteMatchImpl.java
+++ services/backend/match/src/main/java/com/bvb/match/business/DeleteMatchImpl.java
@@ -1,10 +1,12 @@
 package com.bvb.match.business;
 
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
 import com.bvb.match.persistence.Match;
 import com.bvb.match.persistence.MatchRepository;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
@@ -17,9 +19,13 @@ import java.util.List;
 @EnableJms
 public class DeleteMatchImpl implements DeleteMatchService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(DeleteMatchImpl.class);
-
     private final MatchRepository matchRepository;
+
+    @Value("${spring.cloud.azure.servicebus.connection-string}")
+    private String connectionString;
+
+    @Value("${spring.cloud.azure.servicebus.consumer.entity-name}")
+    private String topicName;
 
     @Override
     public void deleteMatch(long matchId) {
@@ -27,6 +33,17 @@ public class DeleteMatchImpl implements DeleteMatchService {
 
         if (match == null) {
             throw new RuntimeException("Match doesn't exist");
+        }
+
+        ServiceBusSenderClient senderClient = new ServiceBusClientBuilder()
+                .connectionString(connectionString)
+                .sender()
+                .topicName(topicName)
+                .buildClient();
+        try {
+            senderClient.sendMessage(new ServiceBusMessage(String.valueOf(matchId)));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to send message", e);
         }
 
         matchRepository.deleteById(matchId);
@@ -40,12 +57,22 @@ public class DeleteMatchImpl implements DeleteMatchService {
         }
     }
 
+    private void getMatchesByTeamId(long teamId) {
+        List<Match> matches = matchRepository.findAllMatchesByHomeTeamIdOrAwayTeamId(teamId, teamId);
+        sendDeletion(matches);
+    }
+
     private void deleteMatchByTournamentId(long tournamentId) {
         List<Match> matches = matchRepository.findAllMatchesByTournamentId(tournamentId);
 
         for (Match m : matches) {
             matchRepository.deleteById(m.getId());
         }
+    }
+
+    private void getMatchesByTournamentId(long tournamentId) {
+        List<Match> matches = matchRepository.findAllMatchesByTournamentId(tournamentId);
+        sendDeletion(matches);
     }
 
     private void deleteMatchBySeasonId(long seasonId) {
@@ -56,27 +83,66 @@ public class DeleteMatchImpl implements DeleteMatchService {
         }
     }
 
+    private void getMatchesBySeasonId(long seasonId) {
+        List<Match> matches = matchRepository.findAllMatchesBySeasonId(seasonId);
+        sendDeletion(matches);
+    }
+
+    private void sendDeletion(List<Match> matches) {
+        ServiceBusSenderClient senderClient = new ServiceBusClientBuilder()
+                .connectionString(connectionString)
+                .sender()
+                .topicName(topicName)
+                .buildClient();
+
+        for (Match m : matches) {
+            try {
+                senderClient.sendMessage(new ServiceBusMessage(String.valueOf(m.getId())));
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to send message", e);
+            }
+        }
+    }
+
     @JmsListener(destination = "deleteteam", containerFactory = "topicJmsListenerContainerFactory",
             subscription = "delete-match-when-team-deletion")
     public void deleteMatchWhenTeamDeletion(byte[] messageBytes) {
         String message = new String(messageBytes, StandardCharsets.UTF_8);
-        LOGGER.info("Message received from team topic: {}", message);
         deleteMatchByTeamId(Long.parseLong(message));
+    }
+
+    @JmsListener(destination = "deleteteam", containerFactory = "topicJmsListenerContainerFactory",
+            subscription = "get-related-matches-when-team-deletion")
+    public void getMatchesWhenTeamDeletion(byte[] messageBytes) {
+        String message = new String(messageBytes, StandardCharsets.UTF_8);
+        getMatchesByTeamId(Long.parseLong(message));
     }
 
     @JmsListener(destination = "deletetournament", containerFactory = "topicJmsListenerContainerFactory",
             subscription = "delete-match-when-tournament-deletion")
     public void deleteMatchWhenTournamentDeletion(byte[] messageBytes) {
         String message = new String(messageBytes, StandardCharsets.UTF_8);
-        LOGGER.info("Message received from tournament topic: {}", message);
         deleteMatchByTournamentId(Long.parseLong(message));
+    }
+
+    @JmsListener(destination = "deletetournament", containerFactory = "topicJmsListenerContainerFactory",
+            subscription = "get-related-matches-when-tournament-deletion")
+    public void getMatchesWhenTournamentDeletion(byte[] messageBytes) {
+        String message = new String(messageBytes, StandardCharsets.UTF_8);
+        getMatchesByTournamentId(Long.parseLong(message));
     }
 
     @JmsListener(destination = "deleteseason", containerFactory = "topicJmsListenerContainerFactory",
             subscription = "delete-match-when-season-deletion")
     public void deleteMatchWhenSeasonDeletion(byte[] messageBytes) {
         String message = new String(messageBytes, StandardCharsets.UTF_8);
-        LOGGER.info("Message received from season topic: {}", message);
         deleteMatchBySeasonId(Long.parseLong(message));
+    }
+
+    @JmsListener(destination = "deleteseason", containerFactory = "topicJmsListenerContainerFactory",
+            subscription = "get-related-matches-when-season-deletion")
+    public void getMatchesWhenSeasonDeletion(byte[] messageBytes) {
+        String message = new String(messageBytes, StandardCharsets.UTF_8);
+        getMatchesBySeasonId(Long.parseLong(message));
     }
 }
--- services/backend/post/src/main/java/com/bvb/post/business/DeletePostImpl.java
+++ services/backend/post/src/main/java/com/bvb/post/business/DeletePostImpl.java
@@ -3,14 +3,20 @@ package com.bvb.post.business;
 import com.bvb.post.persistence.Post;
 import com.bvb.post.persistence.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class DeletePostImpl implements DeletePostService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DeletePostImpl.class);
     private final PostRepository postRepository;
 
     @Override
@@ -31,5 +37,21 @@ public class DeletePostImpl implements DeletePostService {
         }
 
         postRepository.deleteById(postId);
+    }
+
+    @JmsListener(destination = "deletematch", containerFactory = "topicJmsListenerContainerFactory",
+            subscription = "delete-post-when-match-deletion")
+    public void deletePostWhenMatchDeletion(byte[] messageBytes) {
+        String message = new String(messageBytes, StandardCharsets.UTF_8);
+        LOGGER.info("Message received from team topic: {}", message);
+        deletePostByMatchId(Long.parseLong(message));
+    }
+
+    private void deletePostByMatchId(long matchId) {
+        List<Post> posts = postRepository.findAllPostsByMatchId(matchId);
+
+        for (Post p : posts) {
+            postRepository.deleteById(p.getId());
+        }
     }
 }
