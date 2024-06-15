ab94317166949400c444a455d933300940f308cb
Shuhei Hagiwara <498855@student.fontys.nl>
2024-06-15 14:23:18 +0200

    add k6 tests

---
+++ k6-tests/testing/script.js
@@ -0,0 +1,51 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 10 },
+    { duration: '1m', target: 100 },
+    { duration: '2m', target: 500 },
+    { duration: '2m', target: 1000 },
+    { duration: '3m', target: 5000 },  // ramp up to 10 users
+    { duration: '5m', target: 10000 },
+    { duration: '5m', target: 10000 },   // stay at 10 users
+    { duration: '30s', target: 0 },  // ramp down to 0 users
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<3000'], // 95% of requests should be below 300ms
+  }
+};
+
+function userTest() {
+  const url = 'http://108.142.40.148/';
+
+  const res = http.get(url);
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+    'response time < 3 seconds': (r) => r.timings.duration < 3000,
+  });
+  sleep(1);
+}
+
+function loginTest() {
+  const login = {
+    email: "admin@gmail.com",
+    password: "12345678"
+  };
+  const url = 'http://57.153.111.182:8222/api/v1/auth/login';
+  const headers = { 'Content-Type': 'application/json' };
+
+  const res = http.post(url, JSON.stringify(login), { headers: headers });
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+    'response time < 400ms': (r) => r.timings.duration < 3000,
+  });
+  sleep(1);
+}
+
+// Default function
+export default function () {
+  userTest();
+  sleep(1);
+}
