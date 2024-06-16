b2726546f84623e436822c03fb3b0e28acad36d2
Shuhei Hagiwara <498855@student.fontys.nl>
2024-06-16 19:32:40 +0200

    test

--- k6-tests/testing/script.js
+++ k6-tests/testing/script.js
@@ -7,13 +7,13 @@ export const options = {
     { duration: '1m', target: 100 },
     { duration: '2m', target: 500 },
     { duration: '2m', target: 1000 },
-    { duration: '3m', target: 5000 },  // ramp up to 10 users
-    { duration: '5m', target: 10000 },
-    { duration: '5m', target: 10000 },   // stay at 10 users
+    { duration: '3m', target: 5000 },
+    { duration: '5m', target: 10000 },  // ramp up to 10000 users
+    { duration: '5m', target: 10000 },  // stay at 10000 users
     { duration: '30s', target: 0 },  // ramp down to 0 users
   ],
   thresholds: {
-    http_req_duration: ['p(95)<3000'], // 95% of requests should be below 300ms
+    http_req_duration: ['p(95)<3000'], // 95% of requests should be below 3000ms
   }
 };
 
@@ -25,27 +25,24 @@ function userTest() {
     'is status 200': (r) => r.status === 200,
     'response time < 3 seconds': (r) => r.timings.duration < 3000,
   });
-  sleep(1);
 }
 
-function loginTest() {
-  const login = {
-    email: "admin@gmail.com",
-    password: "12345678"
-  };
-  const url = 'http://57.153.111.182:8222/api/v1/auth/login';
-  const headers = { 'Content-Type': 'application/json' };
+function redirectionReview() {
+  const url = 'http://108.142.40.148/create-review';
 
-  const res = http.post(url, JSON.stringify(login), { headers: headers });
+  const res = http.get(url);
   check(res, {
     'is status 200': (r) => r.status === 200,
-    'response time < 400ms': (r) => r.timings.duration < 3000,
+    'response time < 3 seconds': (r) => r.timings.duration < 3000,
   });
-  sleep(1);
 }
 
 // Default function
 export default function () {
+  userTest();
+  sleep(30);
+  redirectionReview();
+  sleep(30);
   userTest();
   sleep(1);
 }
